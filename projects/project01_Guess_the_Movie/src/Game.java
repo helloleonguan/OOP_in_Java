dc195e5d5aaf2c056f1aa70c92ece18dd89d5496
Liyang(Leon) Guan <leonguan1995@gmail.com>
2018-04-29 19:53:46 +1000

    worked on the Game class

--- projects/project01_Guess_the_Movie/src/Game.java
+++ projects/project01_Guess_the_Movie/src/Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Leon on 14/04/2018.
@@ -7,24 +8,32 @@ import java.util.ArrayList;
 
 public class Game {
 
-    private String[] movies = null;
+    private ArrayList<String> movies = null;
     private String selected_movie = "";
     private String current_progress = "";
     private int num_of_wrong_guesses = 0;
-    private ArrayList<String> correct_guesses = new ArrayList<>();
-    private ArrayList<String> wrong_guesses = new ArrayList<>();
+    private ArrayList<Character> correct_guesses = new ArrayList<>();
+    private ArrayList<Character> wrong_guesses = new ArrayList<>();
 
-    public Game(String[] movies) {
-        this.movies = movies;
+    public Game(ArrayList<String> movies) {
+        this.movies = (ArrayList<String>) movies.clone();
     }
 
 
 
-    public void pick_a_movie() {
+    public String pick_a_movie() {
         /**
-         * Randomly picks a movie and return and update the selected movie.
+         * Randomly picks a movie and update the selected_movie & initialize current_progress.
          */
 
+        int num_of_movies = movies.size();
+
+        Random generator = new Random();
+
+        int rand_index = generator.nextInt(num_of_movies);
+        this.selected_movie = movies.get(rand_index);
+
+        return this.selected_movie;
     }
 
     public String output_current_progress() {
@@ -32,13 +41,32 @@ public class Game {
          * Returns the current progress of the game by replacing hidden letter by "_".
          */
 
-        return "";
+        this.current_progress = "";
+
+        for (int i = 0; i < this.selected_movie.length(); i++) {
+            if (this.correct_guesses.contains(this.selected_movie.charAt(i))) {
+                this.current_progress += this.selected_movie.charAt(i);
+            } else {
+                this.current_progress += "_";
+            }
+        }
+
+        return this.current_progress;
     }
 
-    public void guess(String letter) {
+    public boolean guess(Character c) {
         /**
          * Determines whether a guess is correct or not.
          */
+
+        if (this.selected_movie.indexOf(c) >= 0) {
+            this.correct_guesses.add(c);
+            return true;
+        } else {
+            this.wrong_guesses.add(c);
+            this.num_of_wrong_guesses += 1;
+            return false;
+        }
 
     }
 
@@ -47,14 +75,38 @@ public class Game {
          * Determines whether the game has terminated.
          */
 
-        return false;
+        if (num_of_wrong_guesses >= 10) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public void terminate() {
-        /**
-         * Outputs win or lose messages.
-         */
-        
+    public int get_num_of_wrong_guesses() {
+        return this.num_of_wrong_guesses;
     }
 
+    public String get_selected_movie() {
+        return this.selected_movie;
+    }
+
+    public String get_wrong_guesses() {
+        String wrong_guesses_str = "";
+
+        for (Character c : this.wrong_guesses) {
+            wrong_guesses_str += c + " ";
+        }
+
+        return wrong_guesses_str;
+    }
+
+    public String get_correct_guesses() {
+        String correct_guesses_str = "";
+
+        for (Character c : this.correct_guesses) {
+            correct_guesses_str += c + " ";
+        }
+
+        return  correct_guesses_str;
+    }
 }
--- projects/project01_Guess_the_Movie/src/Main.java
+++ projects/project01_Guess_the_Movie/src/Main.java
@@ -1,10 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by Leon on 14/04/2018.
  */
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("movies.txt");
+        Scanner input = new Scanner(file);
+
+        ArrayList<String> movies = new ArrayList<>();
+
+        while (input.hasNextLine()) {
+            movies.add(input.nextLine());
+        }
+
+        Game game = new Game(movies);
+
+        String selected_movie = game.pick_a_movie();
+
+        while (! game.has_end()) {
+            // main interaction
+
+        }
 
     }
 }
